2b23d9dcf38a36b1b515016fe3d93b4d82d14157
Anele Mzinyati <anele.mzinyati@wundermanthompson.com>
2021-09-11 14:54:07 +0200

    Initial commit - project setup, archetype 30, front-endModule - react

---
+++ ui.apps/target/classes/apps/aem-react-spa-app/clientlibs/clientlib-react/resources/precache-manifest.5459e88cb2f34ea36c0802c6a6e84ebf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00cc761e5932ac1570dacede461d38e",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "15bf0b9406c7c0aaa987",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/css/4.8b1faab7.chunk.css"
+  },
+  {
+    "revision": "9d690ddf92da0042408c",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/css/main.af364b2e.chunk.css"
+  },
+  {
+    "revision": "d28d05120f2bf62e2a2f",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/2.2fd92c92.chunk.js"
+  },
+  {
+    "revision": "a112985e346e36813f8faa2e1e345c78",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/2.2fd92c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aabc2b5c2dfc3279339",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/3.b2282c99.chunk.js"
+  },
+  {
+    "revision": "c1a19abb78cf9cde35b8b3bd4cf9adc0",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/3.b2282c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bf0b9406c7c0aaa987",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/4.5c04b9c1.chunk.js"
+  },
+  {
+    "revision": "ea6f1903ee11409afa28",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/5.738537e3.chunk.js"
+  },
+  {
+    "revision": "3898495859854f5051f5",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/6.bacc6708.chunk.js"
+  },
+  {
+    "revision": "9d690ddf92da0042408c",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/main.b6d39c74.chunk.js"
+  },
+  {
+    "revision": "4c644a9eabb32c7f6d2e",
+    "url": "/etc.clientlibs/aem-react-spa-app/clientlibs/clientlib-react/resources/static/js/runtime-main.a634fc05.js"
+  }
+]);
